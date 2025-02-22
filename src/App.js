9d0ccf9b5a59e05abe08c6a5e824eedd05dfde34
HuacongXie <solarsys@outlook.com>
2025-02-22 18:01:22 -0500

    12.4-14 use react-transition-group for animation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 import React, { Component, Fragment} from 'react';
 import './style.css'
-import {CSSTransition} from "react-transition-group";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true
+            list:[]
         }
-        this.handleToggle = this.handleToggle.bind(this);
+        this.handleAddItem = this.handleAddItem.bind(this);
 
     }
     render() {
         return (
             //use Fragment to render as one element
             <Fragment>
-                <CSSTransition
-                    in={this.state.show}
-                    timeout={1000}
-                    classNames='fade'
-                    onEnterd={(el)=>{el.style.color='blue'}}
-                    appear={true}
-                >
+                <TransitionGroup>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+                                <div key={index}> {item}</div>
+                            )
+                        })
+                    }
 
-                    <div >hello</div>
-                </CSSTransition>
+                </TransitionGroup>
 
-                <botton onClick={this.handleToggle}>toggle</botton>
+
+                <botton onClick={this.handleAddItem}>toggle</botton>
 
             </Fragment>
 
         )
     }
-    handleToggle(){
+    handleAddItem(){
         this.setState(
-            {
-                show : this.state.show ? false : true
+            (prevState)=>{
+                return {
+                    list: [...prevState.list, 'item']
+                }
             }
         )
     }
