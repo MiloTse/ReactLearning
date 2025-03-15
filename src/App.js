e730c884dbb1c4d0f8bfb3082cf42a0bc1eacf4e
HuacongXie <solarsys@outlook.com>
2025-03-15 15:20:10 -0400

    1.initial for lesson8 2.remove unrelated files

--- src/App.js
+++ src/App.js
@@ -1,48 +1,22 @@
-import React, { Component, Fragment} from 'react';
-import './style.css'
-import {CSSTransition, TransitionGroup} from "react-transition-group";
+
+import {Component} from "react";
 
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list:[]
-        }
-        this.handleAddItem = this.handleAddItem.bind(this);
+class App   extends Component  {
 
-    }
     render() {
         return (
-            //use Fragment to render as one element
-            <Fragment>
-                <TransitionGroup>
-                    {
-                        this.state.list.map((item,index)=>{
-                            return (
-                                <div key={index}> {item}</div>
-                            )
-                        })
-                    }
-
-                </TransitionGroup>
-
-
-                <botton onClick={this.handleAddItem}>toggle</botton>
-
-            </Fragment>
+            <div  >
+                <header  >
+                    <p>
+                        helloworld
+                    </p>
+                </header>
+            </div>
 
         )
     }
-    handleAddItem(){
-        this.setState(
-            (prevState)=>{
-                return {
-                    list: [...prevState.list, 'item']
-                }
-            }
-        )
-    }
+
 }
 
 export default App;
