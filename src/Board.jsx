85fc3287ad9d2e7af3a34d7fab5b5c151d267687
HuacongXie <solarsys@outlook.com>
2025-03-16 01:01:47 -0400

    1.added logic to calculate winner

--- src/Board.jsx
+++ src/Board.jsx
@@ -9,16 +9,42 @@ function getNextLetter(squares){
     return nextLetter;
 }
 
+function calculateWinner(squares) {
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < winConditions.length; i++) {
+        const [a, b, c] = winConditions[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return 'Winner is : ' + squares[a];
+        }
+    }
+    return null;
+ }
 
     function Board(props) {
         //
         const [squares, setSquares] = useState(Array(9).fill(null));
         const nextLetter = getNextLetter(squares);
-        let status = null;
 
-        if(nextLetter){
+
+
+        const winner = calculateWinner(squares);
+
+        let status = null;
+        if(winner){
+            status = winner;
+        }else if(nextLetter){
             status = `Next player: ${nextLetter}`;
         }
+
 
 
         const clickHandler = (index) =>{
