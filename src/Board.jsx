cd31889a28a6cdc877f8d2a529417403feb506ba
HuacongXie <solarsys@outlook.com>
2025-03-15 20:02:44 -0400

    1.added const nextLetter status 2.split a function

--- src/Board.jsx
+++ src/Board.jsx
@@ -2,19 +2,29 @@ import Square from "./Square";
 import {useState} from "react";
 
 
+function getNextLetter(squares){
+    const filledSquare = squares.filter(item=>(item==='X' || item==='O'));
+    const filledNumber = filledSquare.length;
+    const nextLetter = (filledNumber%2===0)?'X':'O';
+    return nextLetter;
+}
+
 
     function Board(props) {
         //
         const [squares, setSquares] = useState(Array(9).fill(null));
+        const nextLetter = getNextLetter(squares);
+        const status = nextLetter;
+
 
         const clickHandler = (index) =>{
             console.log(index);
 
+
+
             const currentSquares = squares[index];
             if(currentSquares==null){
-                const filledSquare = squares.filter(item=>(item==='X' || item==='O'));
-                const filledNumber = filledSquare.length;
-                const nextLetter = (filledNumber%2===0)?'X':'O';
+
 
                 const newSquares = squares.slice();
                 newSquares[index] = nextLetter;
@@ -25,6 +35,7 @@ import {useState} from "react";
 
         return (
             <>
+                <div>{status}</div>
                 <div className='board-row'>
                     <Square value={squares[0]} index={0} onClick={clickHandler}/>
                     <Square value={squares[1]} index={1} onClick={clickHandler}/>
