f959a18363cc7bd4396aa4e0cb4acb6ee2e8931a
HuacongXie <solarsys@outlook.com>
2025-03-17 00:16:51 -0400

    jsx syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Hello from './Hello.jsx';
 import Bye from './Bye.jsx';
 function App (){
+
+    const text = 'This is text';
     return(
         <>
+            <div>Lesson 12</div>
+            <span>span</span>
             <Hello />
+            <h1>{text}</h1>
             <Bye />
         </>
     );
