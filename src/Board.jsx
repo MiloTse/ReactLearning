c811a41ca3f1880f3a358378b5432d835c3ad5f6
HuacongXie <solarsys@outlook.com>
2025-03-16 21:52:52 -0400

    1. modified class Game to function Game. 2.moved function calculateWinner to Game.jsx 3.used attributes to pass props to child components

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,5 +1,4 @@
 import Square from "./Square";
-import {useState} from "react";
 
 
 function getNextPlay(squares){
@@ -8,35 +7,12 @@ function getNextPlay(squares){
     return (filledNumber%2===0) ? 'X' : 'O';
 }
 
-function calculateWinner(squares) {
-    const winConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < winConditions.length; i++) {
-        const [a, b, c] = winConditions[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    const filledSquare = squares.filter(item=>(item==='X' || item==='O'));
-    if(filledSquare.length===9){
-        return 'No One';
-    }
-    return null;
- }
 
-function Board(props) {
-    const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({squares, winner, setSquares}) {
+    // const [squares, setSquares] = useState(Array(9).fill(null));
     const nextPlay = getNextPlay(squares);
     //determine which letter is the winner
-    const winner = calculateWinner(squares);
+    // const winner = calculateWinner(squares);
     //determine the status of winner of draw next letter
     const status = winner ? `${winner} is winner` : `Next player: ${nextPlay}`;
 /*       let status = null;
