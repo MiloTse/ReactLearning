0f2d0d6bdc14434726fee163c0dde364f3b036c0
HuacongXie <solarsys@outlook.com>
2025-03-18 00:27:24 -0400

    1. use useImmer to add number to list compare to useState. 2.has issue: indexOne add twice in some case

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,43 @@
  import {useState} from "react";
 
 let index = 0;
-
+let indexOne = 0;
  //immutable programming rules:
 function App (){
     const [list, setList ]= useState( []);
+    const [listOne, setListOne ]= useImmer( []);
     function handleClick() {
         const newList = [...list, index];
          setList(newList);
          index = index + 1;
     }
+    function handleListOneClick() {
+
+        setListOne((draft)=>{
+            draft.push(indexOne);
+            indexOne += 1;
+            console.log(123)
+        });
+     }
 
     return (
         <>
             <div onClick={handleClick}>
-                Add Item
+                Add List Item
             </div>
             {
 
                 list.map((item => {
+                    return <div key={item}>{item}</div>
+                }))
+            }
+            <div>------------------------------------------------------------</div>
+            <div onClick={handleListOneClick}>
+                Add Immer List Item
+            </div>
+            {
+
+                listOne.map((item => {
                     return <div key={item}>{item}</div>
                 }))
             }
