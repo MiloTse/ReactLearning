b8c50d9f9d53756aebf51cfc73e685036e8658e1
HuacongXie <solarsys@outlook.com>
2025-03-18 00:33:44 -0400

    1.fix issue: 1.1 comment React.StrictMode on index.js 1.2 remove dependency to outer indexOne variable make it pure function 2. draft now no depend on outer function, everytime passing draft in to function is same

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
  import {useImmer} from 'use-immer';
  import {useState} from "react";
 
-let index = 0;
-let indexOne = 0;
+
  //immutable programming rules:
 function App (){
     const [list, setList ]= useState( []);
     const [listOne, setListOne ]= useImmer( []);
     function handleClick() {
-        const newList = [...list, index];
+        const newList = [...list, list.length];
          setList(newList);
-         index = index + 1;
-    }
+     }
     function handleListOneClick() {
 
         setListOne((draft)=>{
-            draft.push(indexOne);
-            indexOne += 1;
-            console.log(123)
+            draft.push(draft.length);
+             console.log(123)
         });
      }
 
