78c557e324e283484e8ddf94f7bf35c411b6c9ac
HuacongXie <solarsys@outlook.com>
2025-03-18 22:12:58 -0400

    3.10-Diff use Key on list and none list

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,34 @@ import {useState} from "react";
 //当存在Key值时，不管UI节点位置的内容是否变化，只要Key发生了变化，原始组件就会被销毁(高优先级)
 function App() {
     const [to, setTo] =  useState('default');
+    //列表中key值的作用主要是提升列表渲染的效率
+    const [list, setList] = useState([
+        {
+            id: 1,
+            to: 'Warren',
+            content: 'Hello'
+        },
+        {
+            id: 2,
+            value: 'Lily'
+        },
+        {
+            id: 3,
+            value: 'Tom'
+        },
+        {
+            id: 4,
+            value: 'Lucy'
+        },
+    ]);
     console.log(to)
      return (
          <div>
              <Email to={to} key={to}/>
              <button onClick={()=>{setTo('Warren')}}>Send</button>
+             {
+                list.map((item,index)=><p key={item.id}>{item.value}</p>)
+             }
          </div>
     )
 }
