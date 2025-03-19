ea79cddf960487fa6bf397709526f603d5f51abc
HuacongXie <solarsys@outlook.com>
2025-03-18 20:47:51 -0400

    1.concept of declarative programming 2.should avoiding redundant code, setFullName is unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,45 @@
- import {useImmer} from 'use-immer';
- import {useState} from "react";
+
+import {useImmer} from "use-immer";
+import {useState} from "react";
+
+//从命令式开发到声明式开发
+//命令式： 一步步指路， 告诉出租车司机怎么回家
+// 声明式： 告诉出租车司机，你家在哪里，司机会自动把车开到家门口
+// 声明式开发的好处： 1. 代码更加简洁 2. 代码更加易读 3. 代码更加易维护
+// 声明式开发的坏处： 1. 代码更加复杂 2. 代码更加难读 3. 代码更加难维护
+// 声明式开发的例子： React 16.8 之后的 hooks， React 17 之后的 Concurrent Mode， React 18 之后的 Suspense
+// 声明式开发的例子： React 16.8 之前的 setState， React 17 之前的 setState， React 18 之前的 setState
+// 声明式开发的例子： React 16.8 之后的 useReducer， React 17 之后的 useReducer， React 18 之后的 useReducer
+// 声明式开发的例子： React 16.8 之后的 useContext， React 17 之后的 useContext， React 18 之后的 useContext
+// 声明式开发的例子： React 16.8 之后的 useMemo， React 17 之后的 useMemo， React 18 之后的 useMemo
+function App() {
+
+    const [firstName, setFirstName] = useImmer('');
+    const [lastName, setLastName] = useImmer('');
+    //空字符串没必要用immer，用useState就可以了
+    const [fullName, setFullName] = useState('');
 
 
- //immutable programming rules:
-function App (){
-    const [list, setList ]= useState( []);
-    const [listOne, setListOne ]= useImmer( []);
-    function handleClick() {
-        const newList = [...list, list.length];
-         setList(newList);
-     }
-    function handleListOneClick() {
+    function handleFirstNameChange(e){
+      setFirstName(e.target.value);
+      setFullName(e.target.value + '' + lastName);
+    }
+    function handleLastNameChange(e) {
+      setLastName(e.target.value);
+      setFullName(firstName +' ' + e.target.value);
 
-        setListOne((draft)=>{
-            draft.push(draft.length);
-             console.log(123)
-        });
-     }
-
-    return (
-        <>
-            <div onClick={handleClick}>
-                Add List Item
-            </div>
-            {
-
-                list.map((item => {
-                    return <div key={item}>{item}</div>
-                }))
-            }
-            <div>------------------------------------------------------------</div>
-            <div onClick={handleListOneClick}>
-                Add Immer List Item
-            </div>
-            {
-
-                listOne.map((item => {
-                    return <div key={item}>{item}</div>
-                }))
-            }
-        </>
+    }
 
 
-    )
+
+    return <div>
+        First Name:<input onChange={handleFirstNameChange}/>
+        Last Name <input onChange={(e)=> {setLastName(e.target.value)}} />
+        Full Name: {firstName} {lastName}
+        Full Name2: {fullName}
+
+
+    </div>
 }
 
  export default App;
