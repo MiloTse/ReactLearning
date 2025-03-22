512c2d05b6573ee2f646aac0b22898d57c7d7e81
HuacongXie <solarsys@outlook.com>
2025-03-22 19:07:00 -0400

    3.14-Use Context to Pass Values to Deeply Nested Components(add Header component to App component)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-
+import Header from "./Header";
 function App() {
     return (
          <div>
-                Hello World!
+                <Header/>
          </div>
     )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+
+function Header() {
+    return (
+        <div style={{borderBottom: '1px solid black'}}>Header</div>
+    )
+}
+
+export default Header
