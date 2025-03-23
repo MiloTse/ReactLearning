7c07df7b0c1d9f231f3303a7e55b5ea1b0c20efc
HuacongXie <solarsys@outlook.com>
2025-03-23 16:22:05 -0400

    3-16 Practical Tips for Using Context & Passing JSX Content Between Parent and Child Components via Children(If there are nested contexts, the value obtained is from the nearest Provider. For example, it will retrieve the value that applies to the Body component rather than the one that applies to the Header.)

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ function App() {
          <div>
                 <nameContext.Provider value={user}>
                     <Header onChange={handleHeaderChange} />
-                    <Body />
+                    <nameContext.Provider value='hello word'>
+                        <Body />
+                    </nameContext.Provider>
                 </nameContext.Provider>
 
          </div>
