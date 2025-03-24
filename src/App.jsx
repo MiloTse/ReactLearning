f7e58b67f317fa49564261eea55fea2512205800
HuacongXie <solarsys@outlook.com>
2025-03-24 19:37:48 -0400

    4-4 The underlying execution logic of useEffect(added click action on App.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import VideoPlayerOne from "./VideoPlayerOne";
+import {useState} from "react";
 //2.Effect是在render执行结束，，页面更新之后，再执行的
 function App() {
+    const [isPlaying,setIsPlaying] = useState(true);
+
+    function handleBtnClick(){
+        setIsPlaying(!isPlaying);
+    }
     return (
             <div>
-                <VideoPlayerOne />
+                <VideoPlayerOne src="http://localhost:3000/a.mp4" isPlaying={isPlaying}/>
+                <button onClick={handleBtnClick}>
+                   Toggle
+                </button>
             </div>
         )
 }
