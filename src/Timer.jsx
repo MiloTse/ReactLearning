85abe9b241bc7982721218b74f28a1d9e865dd2e
HuacongXie <solarsys@outlook.com>
2025-03-24 19:30:45 -0400

    4-4 The underlying execution logic of useEffect(modified comments )

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -3,7 +3,7 @@ import {useState, useEffect} from "react";
 function Timer() {
     const [time,setTime] =useState((new Date()).getTime());
 
-    //当前代码存在的风险：Timer组件每次展示/隐藏，showTimer=false时，等于null，相当于被销毁了
+    //(If not return clearInterval(timer);) then have a memory leak：Timer组件每次展示/隐藏，showTimer=false时，等于null，相当于被销毁了
     //但是Timer里面有一个定时器，最好在Timer被销毁的时候，定时器也能被销毁，不被销毁有可能导致内存泄漏的问题。
     useEffect(() => {
         //每间隔1s 更新时间
@@ -13,9 +13,9 @@ function Timer() {
         //组件销毁后，清理定时器
         //Clean up the timer after the component is destroyed
         return () => {
+            console.log('clear process...');
             clearInterval(timer);
         }
-
     },[]);
 
     return (
