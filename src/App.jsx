333e72d220e573802a87c70118e3bd942ebeeef6
HuacongXie <solarsys@outlook.com>
2025-03-24 19:09:31 -0400

    4-4 The underlying execution logic of useEffect(created Timer.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 //1.除Effect引l入的临时内容，避免内存泄露
+import Timer from "./Timer";
+
 function App() {
     return (
             <div>
-                Hello World
+                <Timer/>
             </div>
         )
 }
