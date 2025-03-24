e1083adec2d6e61c1a75d8af4387a437b64a99e0
HuacongXie <solarsys@outlook.com>
2025-03-24 18:22:36 -0400

    4-2 Combining Ref with Elements and Components(imported useRef and added click action)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useRef} from "react";
+
 
 function App() {
 
-       return (
+    const inputElement = useRef(null);
+    function handleBtnClick() {
+        console.log('focus');
+        inputElement.current.focus();
+    }
+
+    return (
             <div>
-                Hello World
+               <input ref={inputElement}/>
+                <button onClick={handleBtnClick}>
+                    Focus
+                </button>
             </div>
         )
 }
