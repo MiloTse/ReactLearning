ce52030719fd6c561b48ef7918cd084ea04cc933
HuacongXie <solarsys@outlook.com>
2025-03-24 19:00:17 -0400

    4-3 Usage Scenarios and Basic Steps for Using Effect(Add the age variable for comparison. Clicking the name action only changes the age, and useEffect will not be executed)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import {useEffect, useState} from "react";
 
 function App() {
-    const [name, setName] = useState('warren')
+    const [name, setName] = useState('warren');
+    const [age, setAge] = useState(20);
     //1.使用Effect，首先要用useEffect进行定义，此时，每次render过后，其内容都会执行
     //2.useEffectHook第二个函数参数，可以限定Effect的执行时机，参数值是数组，如果是空数组，表示：只有在第一次渲染的时候执行
     useEffect(()=>{
@@ -10,11 +11,12 @@ function App() {
             console.log('abc')
         });
 
-    },[name])
+    },[name]);
 
 
     function handleNameClick() {
-        setName('warren2');
+        setAge(age + 1);
+        console.log(age);
     }
 
     return (
