41dd77b773c5ab5e1482a2d2fb74885c63d2222a
HuacongXie <solarsys@outlook.com>
2025-03-24 18:51:57 -0400

    4-3 Usage Scenarios and Basic Steps for Using Effect(define setName->render div element->invoke the function in useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [name, setName] = useState('warren')
-    //请求外部数据
-    fetch('http://localhost:3000/a.json').then(()=>{
-        console.log('abc')
+    //1.使用Effect，首先要用useEffect进行定义，此时，每次render过后，其内容都会执行
+
+    useEffect(()=>{
+        //请求外部数据
+        fetch('http://localhost:3000/a.json').then(()=>{
+            console.log('abc')
+        })
+
     })
+
 
     function handleNameClick() {
         setName('warren2');
