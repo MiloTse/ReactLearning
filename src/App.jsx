9e9cea2d4bf2888b476e2856c20ee1a80aa2bc82
HuacongXie <solarsys@outlook.com>
2025-03-24 19:11:59 -0400

    4-4 The underlying execution logic of useEffect(created action on click button and useState)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 //1.除Effect引l入的临时内容，避免内存泄露
 import Timer from "./Timer";
+import {useState} from "react";
 
 function App() {
+    const [showTimer, setShowTimer] = useState(false);
+
+    function handleBtnClick() {
+        const showState = !showTimer;
+        setShowTimer(showState);
+    }
+
     return (
             <div>
                 <Timer/>
+                <button onClick={handleBtnClick}>Toggle</button>
             </div>
         )
 }
