30b3d9478759f10e55fb8d9fd0ab402a1c3d1f88
HuacongXie <solarsys@outlook.com>
2025-03-24 18:48:02 -0400

    4-3 Usage Scenarios and Basic Steps for Using Effect(added click action on name to see the times to render after click name)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import {useState} from "react";
 
 function App() {
+    const [name, setName] = useState('warren')
     //请求外部数据
     fetch('http://localhost:3000/a.json').then(()=>{
         console.log('abc')
     })
 
+    function handleNameClick() {
+        setName('warren2');
+    }
+
     return (
-            <div>
-               Hello World
+            <div onClick={handleNameClick}>
+                {name}
             </div>
         )
 }
