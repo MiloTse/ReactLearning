9055d8d91960f8e6d8ac30a3efa92c3e4f3781bb
HuacongXie <solarsys@outlook.com>
2025-03-24 18:43:36 -0400

    4-3 Usage Scenarios and Basic Steps for Using Effect(App.js added fetch data from url)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 
 function App() {
+    //请求外部数据
+    fetch('http://localhost:3000/a.json').then(()=>{
+        console.log('abc')
+    })
+
     return (
             <div>
                Hello World
