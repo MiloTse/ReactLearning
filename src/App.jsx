804a8552e0604fb42663a5f031eae192afcb79b0
HuacongXie <solarsys@outlook.com>
2025-03-24 19:58:26 -0400

    4-4 The underlying execution logic of useEffect(imported real a.mp4 file to confirm the way use useEffect hook on VideoPlayerTwo.jsx is correct and load video file correctly after clicking the button)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // import VideoPlayerOne from "./VideoPlayerOne";
 import {useState} from "react";
 import VideoPlayerTwo from "./VideoPlayerTwo";
+import video from "./a.mp4";
+
+
 //2.Effect是在render执行结束，，页面更新之后，再执行的
 //2. The Effect is executed after the render has completed and the page has been updated.
+
 function App() {
     //play status
     const [isPlaying,setIsPlaying] = useState(true);
@@ -13,7 +17,8 @@ function App() {
     return (
             <div>
                 {/*<VideoPlayerOne src="http://localhost:3000/a.mp4" isPlaying={isPlaying}/>*/}
-                <VideoPlayerTwo src="http://localhost:3000/a.mp4" isPlaying={isPlaying}/>
+                {/*<VideoPlayerTwo src="http://localhost:3000/a.mp4" isPlaying={isPlaying}/>*/}
+                <VideoPlayerTwo src= {video} isPlaying={isPlaying}/>
                 <button onClick={handleBtnClick}>
                    Toggle
                 </button>
--- src/VideoPlayerTwo.jsx
+++ src/VideoPlayerTwo.jsx
@@ -1,5 +1,8 @@
 import {useEffect, useRef} from "react";
 
+//把ref放在useEffect里面了，执行逻辑变化了。
+//首先会执行useRef,然后执行渲染dom(return (<video ref={ref} src={src} loop/>)),
+//改为了再执行useEffect里面的东西，这时候就不会提示方法不存在，而是资源不存在
 function VideoPlayerTwo({src, isPlaying}) {
     const ref = useRef(null);
 
@@ -11,8 +14,6 @@ function VideoPlayerTwo({src, isPlaying}) {
         }
     })
 
-    return (
-        <video ref={ref} src={src} loop/>
-    )
+    return (<video ref={ref} src={src} loop/>)
 }
 export default VideoPlayerTwo;
