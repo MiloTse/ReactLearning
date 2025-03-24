08e2662c7fac0d9cea384fdb22546f24f4316063
HuacongXie <solarsys@outlook.com>
2025-03-24 14:17:35 -0400

    4-17 Basic usage of Ref(code initialization)

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,7 @@
-import AddItem from './AddItem';
-import ItemList from "./ItemList";
-import {useReducer} from "react";
-import DataContext from "./DataContext";
-import DispatchContext from "./DispatchContext";
-import {useImmerReducer} from "use-immer";
-
-
-function dataReducer(draft, action) {
-    if (action.type === 'changeInput') {
-        draft.inputValue = action.value;
-        return draft;
-    }
-    if (action.type === 'addItem') {
-        draft.list.push({
-            key:draft.inputValue,
-            value: draft.inputValue
-        });
-        draft.inputValue = '';
-        return draft;
-    }
-    if (action.type === 'deleteItem') {
-        draft.list.pop();
-        return draft;
-    }
-    return draft;
-}
 
 function App() {
-    const [data, dispatch] = useImmerReducer(dataReducer, {
-        inputValue: '',
-        list: [],
-    });
-
        return (
-           <>
-               <DataContext.Provider value={data}>
-                   <DispatchContext.Provider value={dispatch}>
-                       <AddItem />
-                       <ItemList />
-                   </DispatchContext.Provider>
-
-               </DataContext.Provider>
-
-           </>
-
-
-    )
+            <div>Hello World</div>
+        )
 }
 export default App;
