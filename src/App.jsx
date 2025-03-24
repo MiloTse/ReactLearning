8964276e8688319ab45cb1a392418cd945acbf1f
HuacongXie <solarsys@outlook.com>
2025-03-24 17:27:54 -0400

    4-17 Basic usage of Ref(added function for click stop button(have a issue: time did not stop))

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import {useState} from "react";
 
 function App() {
     const [time, setTime] = useState((new Date()).getTime());
+    let timer = null;
     function handleStartClick () {
-        setInterval(() => {
+        timer = setInterval(() => {
             setTime((new Date()).getTime())
         }, 1000       )
     }
 
+    function handleStopClick() {
+        clearInterval(timer)
+    }
        return (
             <div>
                 <button onClick={handleStartClick}>Start</button>
-                <button>Stop</button>
+                <button onClick={handleStopClick}>Stop</button>
                 <div>{time}</div>
             </div>
         )
