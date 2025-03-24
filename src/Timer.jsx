4b80e6f8d86ce260c6a552c17b12f1d7ea2ac150
HuacongXie <solarsys@outlook.com>
2025-03-24 19:18:43 -0400

    4-4 The underlying execution logic of useEffect(use useEffect to setInterval for timer)

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,7 +1,15 @@
-import {useState} from "react";
-
+import {useState, useEffect} from "react";
+//1，清除Effect引l入的临时内容，避免内存泄漏
 function Timer() {
     const [time,setTime] =useState((new Date()).getTime());
+    useEffect(() => {
+        //每间隔1s 更新时间
+        const timer = setInterval(() => {
+            setTime((new Date()).getTime());
+        },1000);
+
+    },[]);
+
     return (
         <div>
             {time}
