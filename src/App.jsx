8bb656b1f1f448bef12bd8dd22da8e8af9a8e635
HuacongXie <solarsys@outlook.com>
2025-03-24 20:22:39 -0400

    4. React Advanced Syntax -4.5 Handling the issue of the Effect executing twice in Strict Mode --4.5.1 code initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,6 @@
-// import VideoPlayerOne from "./VideoPlayerOne";
-import {useState} from "react";
-import VideoPlayerTwo from "./VideoPlayerTwo";
-
-
-
-//2.Effect并不是跟随render 属性执行的，而是在render执行结束，页面更新之后，再执行的
-//2. The Effect does not execute alongside the render properties;
-//instead, it runs after the render has completed and the page has been updated.
 
 function App() {
-    //play status
-    const [isPlaying,setIsPlaying] = useState(true);
-
-    function handleBtnClick(){
-        setIsPlaying(!isPlaying);
-    }
     return (
-            <div>
-                {/*<VideoPlayerOne src="http://localhost:3000/a.mp4" isPlaying={isPlaying}/>*/}
-                <VideoPlayerTwo src="https://media.w3.org/2010/05/sintel/trailer.mp4" isPlaying={isPlaying}/>
-
-                <button onClick={handleBtnClick}>
-                   Play/Pause
-                </button>
-            </div>
-        )
-}
+            <div> Hello World </div>
+        )}
 export default App;
