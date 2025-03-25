d54b7a6b0a511ca4b9605ba913d7310a6d820fa2
HuacongXie <solarsys@outlook.com>
2025-03-25 12:42:46 -0400

    4. React Advanced Syntax -4.8 A brief introduction to the component and Effect lifecycle & UseEffectEvent. --4.8.1 created two div and added click actions to change the url and param

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import {useState, useEffect} from 'react';
 
+
+//useEffectEvent
 function App() {
+    const [url, setUrl] = useState('http://localhost:3000');
+    const [param, setParam] = useState('?name=warren');
+
+    useEffect(() => {
+        console.log(`sending request..., address is ${url}${param}`);
+    }, [url, param]);
 
      return (
-            <div>
-               Hello World
-            </div>
-    )
+         <>
+             <div onClick={() => {setUrl('http://localhost:3001')}}>Change Url</div>
+             <div onClick={() => {setParam('?name=milo')}}>Change Param</div>
+         </>
+
+     )
 }
 
 export default App;
