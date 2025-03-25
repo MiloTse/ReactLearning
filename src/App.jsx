2cb61edcbac88fe782009ca07f8c2a69b4020a3c
HuacongXie <solarsys@outlook.com>
2025-03-25 17:23:37 -0400

    4. React Advanced Syntax -4.10 The usage of useCallback, useDebugValue, and useImperativeHandle. --4.10.1 imported useCallback and created input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import {useState, useEffect  } from 'react';
+import {useState, useEffect, useCallback} from 'react';
 
 
 function App() {
-      return (
-        <div>Hello World</div>
+    const [content, setContent] = useState('');
 
+    //第一个是参数是要执行的内容，第二个是要依赖的内容
+    //useCallback返回一个函数，这个函数的执行结果和依赖的内容有关
+    //如果依赖的内容没有改变，那么返回的函数就不会改变
+    const handleContentChange = useCallback((e) => {
+        setContent(e.target.value)
+    }, []);
+
+
+      return (
+        <input value={content ? content: ''} onChange={handleContentChange}/>
      )
 }
 
