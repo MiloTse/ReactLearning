6f08bcbbc196db5446146ddb400e73617bd24f35
HuacongXie <solarsys@outlook.com>
2025-03-25 13:33:50 -0400

    4. React Advanced Syntax -4.10 The usage of useCallback, useDebugValue, and useImperativeHandle. --4.10.1 code initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
 import {useState, useEffect  } from 'react';
 
 
-//自定义Hook
-//1.复复用性会提升
-//2.把分散的逻辑聚集到一起，代码可读性更好
-//必须首字母use开头
-//3，Hook共享的是执行逻辑，多次执行同一个Hook，每次执行对应的状态不是被共享的
-function useUserInfo(){
-    const [userInfo, setUserInfo] = useState({});
-    useEffect(() => {
-        setUserInfo({name:'Warren', job:'Developer'})
-    }, []);
-
-    function changeUserInfo(){
-        setUserInfo({name:'Milo', job:'Engineer'})
-    }
-    return [userInfo, changeUserInfo];
-}
-
 function App() {
-    const [userInfo, changeUserInfo] = useUserInfo({});
-    const [userInfoOne, changeUserInfoOne] = useUserInfo({});
       return (
-         <>
-            <div onClick={changeUserInfo }>{userInfo.name}</div>
-            <div onClick={changeUserInfoOne }>{userInfoOne.name}</div>
-          </>
+        <div>Hello World</div>
 
      )
 }
