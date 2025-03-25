0af4227d11a628b1206ba1a475f00df604cf7fce
HuacongXie <solarsys@outlook.com>
2025-03-25 17:30:09 -0400

    4. React Advanced Syntax -4.10 The usage of useCallback, useDebugValue, and useImperativeHandle. --4.10.1 imported useDebugValue --4.10.2 use useDebugValue but not working due to useDebugValue should be used in custom hooks --4.10.3 created useCallback.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import {useState, useEffect, useCallback} from 'react';
+import {useState,   useCallback, useDebugValue} from 'react';
 
 
+//useCallback 避免render 过程反复生成函数
 function App() {
-    const [content, setContent] = useState('');
-
+    const [content, setContent] = useState('123');
+    //useDebugValue 只能在自定义hook中使用
+    useDebugValue(content);
     //第一个是参数是要执行的内容，第二个是要依赖的内容
     //useCallback返回一个函数，这个函数的执行结果和依赖的内容有关
     //如果依赖的内容没有改变，那么返回的函数就不会改变
