e8af4183deed1eb157d1f460c6b1996a289bd363
HuacongXie <solarsys@outlook.com>
2025-03-25 13:11:28 -0400

    4. React Advanced Syntax -4.9 The purpose and usage of custom Hooks --4.9.1 code initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import {useState, useEffect,useEffectEvent } from 'react';
+import {useState, useEffect  } from 'react';
 
 
-//useEffectEvent
 function App() {
-    const [url, setUrl] = useState('http://localhost:3000');
-    const [param, setParam] = useState('?name=warren');
-
-    const request = useEffectEvent((url)=>{
-        console.log(`sending request..., address is ${url}${param}`);
-    })
-
-    useEffect(() => {
-        request(url);
-    }, [url]);
 
      return (
          <>
-             <div onClick={() => {setUrl('http://localhost:3001')}}>Change Url</div>
-             <div onClick={() => {setParam('?name=milo')}}>Change Param</div>
+
          </>
 
      )
