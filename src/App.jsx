2f8ce58e1a4951bf24964799df53d110a040540c
HuacongXie <solarsys@outlook.com>
2025-03-25 13:29:53 -0400

    4. React Advanced Syntax -4.9 The purpose and usage of custom Hooks --4.9.1 Executing the same customized Hook multiple times does not share the corresponding state each time.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {useState, useEffect  } from 'react';
 //1.复复用性会提升
 //2.把分散的逻辑聚集到一起，代码可读性更好
 //必须首字母use开头
+//3，Hook共享的是执行逻辑，多次执行同一个Hook，每次执行对应的状态不是被共享的
 function useUserInfo(){
     const [userInfo, setUserInfo] = useState({});
     useEffect(() => {
@@ -19,12 +20,12 @@ function useUserInfo(){
 
 function App() {
     const [userInfo, changeUserInfo] = useUserInfo({});
-    const [parentInfo, setParentInfo] = useState({name:'dad'});
-     return (
+    const [userInfoOne, changeUserInfoOne] = useUserInfo({});
+      return (
          <>
             <div onClick={changeUserInfo }>{userInfo.name}</div>
-             <div onClick={()=>{setParentInfo({name:'mom'})}}>{parentInfo.name}</div>
-         </>
+            <div onClick={changeUserInfoOne }>{userInfoOne.name}</div>
+          </>
 
      )
 }
