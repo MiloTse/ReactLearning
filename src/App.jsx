6246aa564dbab0f4d7200c02ff95c9094fa13c78
HuacongXie <solarsys@outlook.com>
2025-03-25 13:25:09 -0400

    4. React Advanced Syntax -4.9 The purpose and usage of custom Hooks --4.9.1 added comment to explain the purpose of custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import {useState, useEffect  } from 'react';
 
+
+//自定义Hook
+//1.复复用性会提升
+//2.把分散的逻辑聚集到一起，代码可读性更好
+//必须首字母use开头
 function useUserInfo(){
     const [userInfo, setUserInfo] = useState({});
     useEffect(() => {
@@ -14,13 +19,11 @@ function useUserInfo(){
 
 function App() {
     const [userInfo, changeUserInfo] = useUserInfo({});
-    const [parentInfo, setParentInfo] = useState({name:'aaa'});
-
-
+    const [parentInfo, setParentInfo] = useState({name:'dad'});
      return (
          <>
             <div onClick={changeUserInfo }>{userInfo.name}</div>
-             <div onClick={()=>{setParentInfo({name:'bbb'})}}>{parentInfo.name}</div>
+             <div onClick={()=>{setParentInfo({name:'mom'})}}>{parentInfo.name}</div>
          </>
 
      )
