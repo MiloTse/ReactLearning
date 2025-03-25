b56ff5d1268467f817d2d8a627f8a6b9420c2f40
HuacongXie <solarsys@outlook.com>
2025-03-25 12:11:06 -0400

    4. React Advanced Syntax -4.7 The usage of useSyncExternalStore --4.7.1 use useSyncExternalStore hook to subscribe external data source which is window.navigator.onLine

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import {useEffect, useState} from "react";
+import {useSyncExternalStore} from "react";
+
 
 
 //如果一个数据内容是来自于外部系统的，使用useSyncExternalState来实现，而不要使用下面这种实现方式
 //因为这种方式会导致组件的状态和外部系统的状态不一致
 //If a data content comes from an external system, use useSyncExternalState instead of this implementation
 //That's because it leads to an inconsistency between the state of the component and the state of the external system
-function App() {
-    const [isOnline, setIsOnline] = useState(true);
-    function updateState(){
-        setIsOnline(window.navigator.onLine);
-    }
-    useEffect(() => {
-        window.addEventListener("online", updateState);
-        window.addEventListener("offline", updateState);
 
-        return () => {
-            window.removeEventListener("online", handleOnline);
-            window.removeEventListener("offline", handleOffline);
-        }
-    });
+function subscribe(callback)  {
+    window.addEventListener("online", callback);
+    window.addEventListener("offline", callback);
+    return () => {
+        window.removeEventListener("online", callback);
+        window.removeEventListener("offline", callback);
+    }
+}
+
+function App() {
+    const isOnline = useSyncExternalStore(subscribe, ()=>window.navigator.onLine );
 
      return (
             <div>
