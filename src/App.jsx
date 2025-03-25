ec93dd1150909ec48219dd5161c20292f1c286f6
HuacongXie <solarsys@outlook.com>
2025-03-25 11:03:51 -0400

    4. React Advanced Syntax -4.6 useMemo usage scenarios and usage methods --4.6.1 added simple input and select label

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 
 function App() {
     return (
-            <div > Hello World</div>
-)}
+            <div >
+                <div>
+                    name: <input/>
+                </div>
+                <div>
+                    search: <input/>
+                </div>
+                <div>
+                    <select>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                    </select>
+                </div>
+
+
+            </div>
+    )
+}
+
 export default App;
 
 
