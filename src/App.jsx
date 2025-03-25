fb8f2b53b4ab43277500e600fc04f712ed10cfd3
HuacongXie <solarsys@outlook.com>
2025-03-25 13:18:18 -0400

    4. React Advanced Syntax -4.9 The purpose and usage of custom Hooks --4.9.1 added variable parentInfo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {useState, useEffect  } from 'react';
 
 function App() {
     const [userInfo, setUserInfo] = useState({});
+    const [parentInfo, setParentInfo] = useState({name:'aaa'});
+
     useEffect(() => {
         setUserInfo({name:'Warren', job:'Developer'})
     }, []);
@@ -10,6 +12,7 @@ function App() {
      return (
          <>
             <div onClick={()=>{setUserInfo({name:'Milo', job:'Engineer'})}}>{userInfo.name}</div>
+             <div onClick={()=>{setParentInfo({name:'bbb'})}}>{parentInfo.name}</div>
          </>
 
      )
