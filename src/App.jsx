624ab25f835d47ab80d783b10a184c00afc68a11
HuacongXie <solarsys@outlook.com>
2025-03-25 12:33:17 -0400

    4. React Advanced Syntax -4.8 A brief introduction to the component and Effect lifecycle & UseEffectEvent. --4.8.1 comment explaining the difference between Event and Effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-//组件存在的整个过程称为一个生命周期
+//1.组件存在的整个过程称为一个生命周期
 //挂载（mount）->更新（update）->卸载（unmount）
-//effect 存在的整个过程称为一个生命周期
-//创建执行->更新执行->卸载执行
+//组件的挂载和销毁只有一次
+//2.Effect 存在的整个过程称为一个生命周期
+//创建执行-> 销毁
 //组件里的effect 的挂载和销毁可能会有很多次
+//3.Effect 和 Event 的区别是什么呢？
+//Event 是用户行为，Effect 是组件行为。
+//Effect 是依赖的数据发生变化的时候执行，有时是依赖用户行为，有时是依赖于定时器
+//Effect 是有依赖数据驱动执行的，Event 一定是用户行为驱动执行的。
 import {useEffect, useState} from "react";
 
 function App() {
@@ -14,6 +19,7 @@ function App() {
         }
     }, [userName]);
 
+    console.log('render');
      return (
             <div onClick={()=>{setUserName('MiloTse')}}>
                 {userName}
