0bd6b24a3edbf9fe78ca537357887253e16b7b3f
HuacongXie <solarsys@outlook.com>
2025-03-25 12:20:20 -0400

    4. React Advanced Syntax -4.8 A brief introduction to the component and Effect lifecycle & UseEffectEvent. --4.8.1 use useEffect to show the component lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+//组件存在的整个过程称为一个生命周期
+//挂载（mount）->更新（update）->卸载（unmount）
+//effect 存在的整个过程称为一个生命周期
+//创建执行->更新执行->卸载执行
+//组件里的effect 的挂载和销毁可能会有很多次
+import {useEffect, useState} from "react";
 
 function App() {
+    const [userName, setUserName] = useState('Warren');
+    useEffect(() => {
+        console.log('execute  ');
+        return () => {
+            console.log('destroy  ');
+        }
+    }, [userName]);
+
      return (
-            <div>
-               Hello World
+            <div onClick={()=>{setUserName('MiloTse')}}>
+                {userName}
             </div>
     )
 }
