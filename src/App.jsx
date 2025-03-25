f5a7c44f7a3d1110484c6dd6f997459bc2c1c12f
HuacongXie <solarsys@outlook.com>
2025-03-25 01:22:57 -0400

    4. React Advanced Syntax -4.5 Handling the issue of the Effect executing twice in Strict Mode --4.5.1 setInterval in useEffect executed 2 times in Strict Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 //StrictMode严格模式，在Effect中的执行逻辑
+import {useEffect, useState} from "react";
+
 function App() {
-    console.log('App Render');
+    const [ time, setTime ] = useState((new Date()).getTime());
+
+    //render函数渲染完成后，useEffect会开始判断是否执行
+    useEffect(() => {
+        setInterval(()=>{
+            console.log("useEffect")
+            setTime((new Date()).getTime())
+        },1000)
+    }, [])
+
+
     return (
-            <div> Hello World </div>
+            <div> {time} </div>
         )}
 export default App;
