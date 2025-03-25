e68648de8936e7c5bb96a398b1ba8ba7297c477a
HuacongXie <solarsys@outlook.com>
2025-03-25 13:23:21 -0400

    4. React Advanced Syntax -4.9 The purpose and usage of custom Hooks --4.9.1 created customized hook useUserInfo instead of using useSate on userInfo and changeUserInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import {useState, useEffect  } from 'react';
 
-
-function App() {
+function useUserInfo(){
     const [userInfo, setUserInfo] = useState({});
-    const [parentInfo, setParentInfo] = useState({name:'aaa'});
-
     useEffect(() => {
         setUserInfo({name:'Warren', job:'Developer'})
     }, []);
 
+    function changeUserInfo(){
+        setUserInfo({name:'Milo', job:'Engineer'})
+    }
+    return [userInfo, changeUserInfo];
+}
+
+function App() {
+    const [userInfo, changeUserInfo] = useUserInfo({});
+    const [parentInfo, setParentInfo] = useState({name:'aaa'});
+
+
      return (
          <>
-            <div onClick={()=>{setUserInfo({name:'Milo', job:'Engineer'})}}>{userInfo.name}</div>
+            <div onClick={changeUserInfo }>{userInfo.name}</div>
              <div onClick={()=>{setParentInfo({name:'bbb'})}}>{parentInfo.name}</div>
          </>
 
