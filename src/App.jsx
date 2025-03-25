99834ecb6c248b598cd2288323a6945a545c4021
HuacongXie <solarsys@outlook.com>
2025-03-25 13:15:59 -0400

    4. React Advanced Syntax -4.9 The purpose and usage of custom Hooks --4.9.1 added logic to change userInfo

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import {useState, useEffect  } from 'react';
 
 
 function App() {
+    const [userInfo, setUserInfo] = useState({});
+    useEffect(() => {
+        setUserInfo({name:'Warren', job:'Developer'})
+    }, []);
 
      return (
          <>
-
+            <div onClick={()=>{setUserInfo({name:'Milo', job:'Engineer'})}}>{userInfo.name}</div>
          </>
 
      )
