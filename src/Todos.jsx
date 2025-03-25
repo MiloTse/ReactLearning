62be74dda64d1fbeab648bbbe8b7cfd56e0e3960
HuacongXie <solarsys@outlook.com>
2025-03-25 19:38:09 -0400

    4. React Advanced Syntax -4.11 In-depth explanation of the built-in components Profiler and Suspense. --4.11.1 created Todos.jsx for explanation of Suspense

---
+++ src/Todos.jsx
@@ -0,0 +1,13 @@
+
+
+function Todos(){
+    return (
+        <div>
+            <p>Todo One</p>
+            <p>Todo Two</p>
+            <p>Todo Three</p>
+        </div>
+    )
+}
+
+export default Todos;
