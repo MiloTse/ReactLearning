b8461746d4a4f8e5945e47e14babbed223734272
HuacongXie <solarsys@outlook.com>
2025-03-25 20:22:41 -0400

    4. React Advanced Syntax -4.11 In-depth explanation of the built-in components Profiler and Suspense. --4.11.1 added first param to Promise and setTimeout to change promiseStatus to 'resolved' --4.11.2 after 1000ms, 'Loading' become 'Helle World' --4.11.3. end of chapter 4.11

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,11 +1,29 @@
-
-const promise = new Promise(()=>{}, ()=>{});
+let promiseStatus = "pending";
+//第一个参数是触发函数
+//第二个参数是拒绝函数
+const promise = new Promise((resolver)=>{
+    setTimeout(()=>{
+        resolver();
+        promiseStatus = "resolved";
+    }, 1000);
+}, ()=>{});
 
 function getData(){
-    throw promise;
+    if(promiseStatus === "pending") {
+        throw promise;
+    }else {
+        return 'Hello World';
+    }
+
+
 }
 function Todos(){
-    const data = getData();
+    //当发现throw promise的时候，会把promise作为错误对象抛出
+    //每间隔一段时间就会重新执行getData函数
+    //直到promise被resolve或者reject
+    //如果promise被resolve，那么getData函数就会返回promise的结果
+    //如果promise被reject，那么getData函数就会抛出promise的错误对象
+     const data = getData();//throw promise
     return (
         <div>
             <p>{data}</p>
