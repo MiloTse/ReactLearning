e063cae1720b086488d4d3c39c1c1982962ce134
HuacongXie <solarsys@outlook.com>
2025-03-26 01:46:19 -0400

    4. React Advanced Syntax -4.12 Using useDeferredValue to enhance user experience. --4.12.1 imported memo and use DeferredValue and memo hook to defer the rendering of a component, which can improve the performance of the application by reducing the number of re-renders. --4.12.2 user input can be quick and render being deferred.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import {useDeferredValue, useState} from "react";
+import {useDeferredValue, useState, memo} from "react";
 
-function Todos ({text}){
+const Todos = memo (({text}) =>{
     const items = [];
     for (let i = 0; i < 100; i++) {
         items.push(<div key={i}>{text}</div>)
     }
-
-    const startime = (new Date()).getTime();
-
-    while((new Date()).getTime() - startime < 60){
-    }
+    const starTime = (new Date()).getTime();
+    while((new Date()).getTime() - starTime < 60){}
     return <div>{items}</div>
-
-}
+})
 
 function App() {
     const[inputValue,setInputValue]= useState('');
@@ -28,7 +24,6 @@ function App() {
         </>
     );
 }
-
 export default App;
 
 
