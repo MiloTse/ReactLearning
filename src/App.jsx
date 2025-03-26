fe28f67ac856365ee38fd60bc1873c3193c54e6e
HuacongXie <solarsys@outlook.com>
2025-03-26 02:15:18 -0400

    4. React Advanced Syntax -4.13 Detailed explanation of the usage of useTransition and memo. --4.13.1 created Child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import {useState} from "react";
+
+function Child({name}) {
+    console.log('Child render');
+    return  <div>{name}</div>
+
+}
 
 function App() {
-
+    const [name,setName ]= useState('');
     return (
         <>
+            <div>
+                name:<input value={name || ''} onChange={(e)=> {setName(e.target.value)}}/>
+            </div>
+            <Child name={name}></Child>
 
         </>
     );
 }
+
 export default App;
 
 
