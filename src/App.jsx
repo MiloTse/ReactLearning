62f0b34af8fdbe3b0067558f647bcdfbd91748be
HuacongXie <solarsys@outlook.com>
2025-03-26 01:29:02 -0400

    4. React Advanced Syntax -4.12 Using useDeferredValue to enhance user experience. --4.12.1 imported useDeferredValue from React. --4.12.2 input value to input field was still slow even if used useDeferredValue.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useDeferredValue, useState} from "react";
 
 function Todos ({text}){
     const items = [];
@@ -9,20 +9,22 @@ function Todos ({text}){
     const startime = (new Date()).getTime();
 
     while((new Date()).getTime() - startime < 60){
-        return <div>{items}</div>
     }
+    return <div>{items}</div>
 
 }
 
 function App() {
     const[inputValue,setInputValue]= useState('');
+    //defer inputValue
+    const deferredInputValue = useDeferredValue(inputValue);
     return (
         <>
             <input
                 value={inputValue || ''}
                 onChange={(e)=>{setInputValue(e.target.value)}}
             />
-            <Todos text={inputValue}/>
+            <Todos text={deferredInputValue}/>
         </>
     );
 }
