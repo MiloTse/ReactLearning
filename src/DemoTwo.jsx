363c1b017f157db6510773b8e67694366cb653f0
HuacongXie <solarsys@outlook.com>
2025-03-26 01:47:52 -0400

    4. React Advanced Syntax -4.12 Using useDeferredValue to enhance user experience. --4.12.1 moved to new created DemoTwo.jsx --4.12.2 end of 4.12

---
+++ src/DemoTwo.jsx
@@ -0,0 +1,62 @@
+import {useDeferredValue, useState, memo} from "react";
+
+const Todos = memo (({text}) =>{
+    const items = [];
+    for (let i = 0; i < 100; i++) {
+        items.push(<div key={i}>{text}</div>)
+    }
+    const starTime = (new Date()).getTime();
+    while((new Date()).getTime() - starTime < 60){}
+    return <div>{items}</div>
+})
+
+function App() {
+    const[inputValue,setInputValue]= useState('');
+    //defer inputValue
+    const deferredInputValue = useDeferredValue(inputValue);
+    return (
+        <>
+            <input
+                value={inputValue || ''}
+                onChange={(e)=>{setInputValue(e.target.value)}}
+            />
+            <Todos text={deferredInputValue}/>
+        </>
+    );
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
