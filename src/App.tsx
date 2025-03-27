85e89729977a61fcb7dde41d6031fde6e0f4e676
HuacongXie <solarsys@outlook.com>
2025-03-27 00:41:48 -0400

    5.Common TypeScript type definitions in React. -5.2 Handling Type Issues Related to Hooks (Part 1) --5.2.1 code initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
 //TypeScript是给每一个变量、形式参数、函数(入参和返回值)定义明确的类型
-//1-能够提供更好的语法提示
-//2.能够检测出潜在的问题
-//实际参数不需要类型，形式参数需要类型
-import {JSX} from "react";
-type ChildProps = {
-    age: number;
-    dom: JSX.Element
-}
-//函数组件，Props类型如何定义
-function Child( { age ,dom}: ChildProps) {
-
-    return (
-    <>
-        <div>{age.toString()}</div>
-        {dom}
-    </>
-    );
-
-}
-
+//Hook 相关的类型定义
 function App() {
-    const dom = <div>dom</div>
   return (
     <div>
-        <Child age={18} dom={dom} />
+
     </div>
   );
 }
