ea253e15b354eba074bcba1cbb2bce777c173a8d
HuacongXie <solarsys@outlook.com>
2025-03-27 00:46:01 -0400

    5.Common TypeScript type definitions in React. -5.2 Handling Type Issues Related to Hooks (Part 1) --5.2.1 no difference between use typescript with react, but ts has hint on String type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 //TypeScript是给每一个变量、形式参数、函数(入参和返回值)定义明确的类型
 //Hook 相关的类型定义
+import {useState} from "react";
+
 function App() {
+  const [name, setName] = useState('Warren');
   return (
     <div>
-
+      <button onClick={() => {setName('XXX')}}>Button</button>
+      <div>{name}</div>
     </div>
   );
 }
