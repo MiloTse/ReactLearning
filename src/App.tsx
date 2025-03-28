00488f1ac6b222ab1a8e80b2986c7f0934009a63
HuacongXie <solarsys@outlook.com>
2025-03-28 17:04:08 -0400

    5.Common TypeScript type definitions in React. -5.2 Handling Type Issues Related to Hooks (Part 2) --5.2.1 code initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 //TypeScript是给每一个变量、形式参数、函数(入参和返回值)定义明确的类型
 //Hook 相关的类型定义
-import {useEffect, useRef} from "react";
+
 
 function App() {
-    const ref = useRef<number | null>(null!);
-    useEffect(()=>{
-        ref.current = window.setTimeout(()=>{
-            console.log('timer');
-        },1000);
-        return clearTimeout(ref.current);
-    },[])
     return (
-        <>
-
-            <div >
-                123
-            </div>
-        </>
-
+        <div>
+            hello world
+        </div>
     );
 }
 
