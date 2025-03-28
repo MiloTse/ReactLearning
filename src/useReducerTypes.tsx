aedf5eb080c7ca229f676f1f5877f8ca89e38bf3
HuacongXie <solarsys@outlook.com>
2025-03-28 16:51:56 -0400

    5.Common TypeScript type definitions in React. -5.2 Handling Type Issues Related to Hooks (Part 1) --5.2.1 imported useRef --5.2.2 show error: as ref.current does know ref is html

--- src/useReducerTypes.tsx
+++ src/useReducerTypes.tsx
@@ -1,34 +1,18 @@
 //TypeScript是给每一个变量、形式参数、函数(入参和返回值)定义明确的类型
 //Hook 相关的类型定义
-import {useReducer } from "react";
-
-type ActionType = {
-    type: 'ADD';
-}
-
-const initialState = {
-    count: 0
-}
-
-function reducer(state: typeof initialState, action: ActionType) {
-    const newState = {...state};
-    if(action.type === 'ADD') {
-    newState.count += 1;
-    }
-    return newState;
-}
+import {useRef} from "react";
 
 function App() {
-   const [state, dispatch] = useReducer(reducer, initialState);
+ const ref = useRef(null);
 
+ function handleDivClick() {
+   console.log(ref.current.innerHTML);
+ }
   return (
       <>
-          <button onClick={() => {
-            dispatch({type: 'ADD'})
-          }}>ADD</button>
 
-          <div>
-              {state.count}
+          <div ref={ref} onClick={handleDivClick}>
+              123
           </div>
       </>
 
