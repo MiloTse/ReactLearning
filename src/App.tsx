3469a9d23895662f81d752e52d86544e9dae85c2
HuacongXie <solarsys@outlook.com>
2025-03-28 17:01:23 -0400

    5.Common TypeScript type definitions in React. -5.2 Handling Type Issues Related to Hooks (Part 1) --5.2.1 show error: Type number is not assignable to type

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 //TypeScript是给每一个变量、形式参数、函数(入参和返回值)定义明确的类型
 //Hook 相关的类型定义
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
 function App() {
-    const ref = useRef<HTMLDivElement>(null!);
-
-    function handleDivClick() {
-        console.log(ref.current.innerHTML);
-    }
+    const ref = useRef(null!);
+    useEffect(()=>{
+        ref.current = window.setTimeout(()=>{
+            console.log('timer');
+        },1000);
+        return clearTimeout(ref.current);
+    },[])
     return (
         <>
 
-            <div ref={ref} onClick={handleDivClick}>
+            <div >
                 123
             </div>
         </>
