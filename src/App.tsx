631d72d13b5518b01a833a68bd03c6a2c706733e
HuacongXie <solarsys@outlook.com>
2025-03-28 17:15:05 -0400

    5.Common TypeScript type definitions in React. -5.2 Handling Type Issues Related to Hooks (Part 2) --5.2.1 define for useImperativeHandle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 //TypeScript是给每一个变量、形式参数、函数(入参和返回值)定义明确的类型
-//Hook 相关的类型定义
+//Hook 相关的类型定义, forwardRef, useImperativeHandle
+
+import {forwardRef, ReactNode, useImperativeHandle, useRef} from "react";
+
+type PropsType = {
+    children:  ReactNode
+}
+type RefType = {
+    start: () => void;
+}
+const Child = forwardRef<any,PropsType>((props, ref)=>{
+  useImperativeHandle(ref,()=>{
+     return {
+         start(){
+             console.log("start")
+         }
+     }
+  })
+    return <div>{props.children}</div>
+});
+
+
 
 
 function App() {
+    const ref = useRef<RefType>(null!);
     return (
-        <div>
-            hello world
-        </div>
+        <>
+            <button onClick={()=>{
+                ref.current.start()
+            }}>Click</button>
+            <Child ref={ref}>Hello World</Child>
+        </>
+
     );
 }
 
