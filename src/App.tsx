006f393e836c5624db65bae45fe1c0cd32241066
HuacongXie <solarsys@outlook.com>
2025-03-28 15:12:24 -0400

    5.Common TypeScript type definitions in React. -5.2 Handling Type Issues Related to Hooks (Part 1) --5.2.1 another way: add ? on type User and remove null on useState

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@
 import {useState} from "react";
 
 type User = {
-    name: string;
+    name?: string;
 }
 
 
 function App() {
     //useState<User | null> 中的类型是User或者null
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User>({})
   return (
     <div>
       <button onClick={() => {setUser({name: 'zhangsan'})}}>Button</button>
