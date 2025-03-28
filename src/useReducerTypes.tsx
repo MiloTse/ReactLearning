803a20b6923a9b8d0ccaadf911d82cfacd46bec7
HuacongXie <solarsys@outlook.com>
2025-03-28 16:48:18 -0400

    5.Common TypeScript type definitions in React. -5.2 Handling Type Issues Related to Hooks (Part 1) --5.2.1 App.tsx move to useReducerTypes.tsx

---
+++ src/useReducerTypes.tsx
@@ -0,0 +1,38 @@
+//TypeScript是给每一个变量、形式参数、函数(入参和返回值)定义明确的类型
+//Hook 相关的类型定义
+import {useReducer } from "react";
+
+type ActionType = {
+    type: 'ADD';
+}
+
+const initialState = {
+    count: 0
+}
+
+function reducer(state: typeof initialState, action: ActionType) {
+    const newState = {...state};
+    if(action.type === 'ADD') {
+    newState.count += 1;
+    }
+    return newState;
+}
+
+function App() {
+   const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+      <>
+          <button onClick={() => {
+            dispatch({type: 'ADD'})
+          }}>ADD</button>
+
+          <div>
+              {state.count}
+          </div>
+      </>
+
+  );
+}
+
+export default App;
