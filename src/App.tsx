8722737c175cdf34e9757f3eb3bec395f0e17579
HuacongXie <solarsys@outlook.com>
2025-03-28 16:47:04 -0400

    5.Common TypeScript type definitions in React. -5.2 Handling Type Issues Related to Hooks (Part 1) --5.2.1 used useReducer to how to handle the type of ts grammar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 //TypeScript是给每一个变量、形式参数、函数(入参和返回值)定义明确的类型
 //Hook 相关的类型定义
-import {useState} from "react";
+import {useReducer } from "react";
 
-type User = {
-    name?: string;
+type ActionType = {
+    type: 'ADD';
 }
 
+const initialState = {
+    count: 0
+}
+
+function reducer(state: typeof initialState, action: ActionType) {
+    const newState = {...state};
+    if(action.type === 'ADD') {
+    newState.count += 1;
+    }
+    return newState;
+}
 
 function App() {
-    //useState<User | null> 中的类型是User或者null
-  const [user, setUser] = useState<User>({})
+   const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div>
-      <button onClick={() => {setUser({name: 'zhangsan'})}}>Button</button>
-      <div>{user?.name}</div>
-    </div>
+      <>
+          <button onClick={() => {
+            dispatch({type: 'ADD'})
+          }}>ADD</button>
+
+          <div>
+              {state.count}
+          </div>
+      </>
+
   );
 }
+
 export default App;
