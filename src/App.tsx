2b690340b60df07df8084c8efdedb4c4c0afe43c
HuacongXie <solarsys@outlook.com>
2025-03-29 00:51:16 -0400

    5.Common TypeScript type definitions in React. -5.4 TypeScript type definitions for React-related events. --5.4.1 diff of with or without <HTMLInputElement>

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ const App = ()=> {
     function handleClick(e: React.MouseEvent) {
         console.log(e.currentTarget.innerHTML);
     }
+
+    //define the changeEvent was triggered by input element
     function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
         console.log(e.target.value);
+    }
+    function handleInputChange2(e: React.ChangeEvent) {
+        console.log(e.currentTarget.nodeValue);// output is null;
     }
 
     return (
@@ -12,6 +17,7 @@ const App = ()=> {
             <div>
                 <div onClick={handleClick}>Hello World</div>
                 <input onChange={handleInputChange}/>
+                <input onChange={handleInputChange2}/>
             </div>
 
         </>
